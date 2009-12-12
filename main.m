cdeb433c584402865873916b7e8daa13d7982977
Patrick Tsai <yinghau76@gmail.com>
2009-12-12 22:34:10 +0800

    ADD: more project files

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MyTube
+//
+//  Created by patrick on 2009/12/12.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
